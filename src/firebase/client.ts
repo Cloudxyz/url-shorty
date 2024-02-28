166a81d8af54a47cb65e912a63ecf62f1a310b62
Cloudz <cloudzeroxyz@gmail.com>
2024-02-28 17:56:30 -0600

    add firebase to project

---
+++ src/firebase/client.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCnbFa9fBS0VVfy_SmBSSanWxERtzhB_CQ",
+  authDomain: "shorty-ae5f6.firebaseapp.com",
+  projectId: "shorty-ae5f6",
+  storageBucket: "shorty-ae5f6.appspot.com",
+  messagingSenderId: "990766889229",
+  appId: "1:990766889229:web:613c3d4174f00ae51b1844"
+};
+
+export const app = initializeApp(firebaseConfig);
