cd6ab55a804601183b7c1f8849413c6c9f9a9d1e
Cloudz <cloudzeroxyz@gmail.com>
2024-02-29 17:51:03 -0600

    Add Firebase

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,7 +8,7 @@ interface ImportMetaEnv {
   readonly FIREBASE_CLIENT_ID: string;
   readonly FIREBASE_AUTH_URI: string;
   readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string
+  readonly FIREBASE_AUTH_CERT_URL: string;
   readonly FIREBASE_CLIENT_CERT_URL: string;
 }
 
